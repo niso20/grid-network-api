920665e2c4f5b2900ae81f0d7eb4b7af1ebfd501
niso20 <akalojob@gmail.com>
2025-05-29 19:59:53 +0100

    Add files via upload

---
+++ transformers.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE transformers CASCADE;
+
+INSERT INTO "transformers" ("id", "name", "manufacturer_id", "serial_no", "power_rating", "power_rating_unit", "type_of_cooling", "voltage_rating", "manufacture_year", "installation_year", "station_id", "created_at", "updated_at") VALUES (1, 'TO1', 1, '089564321257', 150, 'mva', 'OFOA', '330/132/33KV', 2010, 2024, 1, '2025-05-28 19:49:46.006085', '2025-05-28 19:49:46.006085');
