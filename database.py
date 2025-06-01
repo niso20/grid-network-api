cef6a15a694fc1816d207710816698dbb09df719
Akachukwu Aneke <akalodave@gmail.com>
2025-06-02 00:57:37 +0100

    updated Database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosApp.db'
 # SQLALCHEMY_DATABASE_URL = 'postgresql://princeak:dRKH5CsZC6Lm4RMggNkjmkr79g25T23u@dpg-d0cacsi4d50c73but2tg-a/powergrid'
 # SQLALCHEMY_DATABASE_URL = 'postgresql://princeak@localhost/powerGrid'
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
